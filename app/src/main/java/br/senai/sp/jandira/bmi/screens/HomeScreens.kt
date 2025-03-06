ec50f24696b7de82d759f8f2ed7c48066bbcdc60
FELIPE BAHIA CORREA <24122657@dsn127m1249386.cfp127aluno.local>
2025-03-06 17:14:15 -0300

    atualizando a atividade

--- app/src/main/java/br/senai/sp/jandira/bmi/screens/HomeScreens.kt
+++ app/src/main/java/br/senai/sp/jandira/bmi/screens/HomeScreens.kt
@@ -1,6 +1,5 @@
 package br.senai.sp.jandira.bmi.screens
 
-import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -11,15 +10,19 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowForward
-import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -27,6 +30,8 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -34,6 +39,11 @@ import br.senai.sp.jandira.bmi.R
 
 @Composable
 fun HomeScreens(modifier: Modifier = Modifier) {
+
+    var nomeState = remember {
+        mutableStateOf(value = "")
+    }
+
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -94,14 +104,34 @@ fun HomeScreens(modifier: Modifier = Modifier) {
                         .padding(start = 35.dp)
                 )
                 OutlinedTextField(
-                    modifier = Modifier
-                        .padding(start = 30.dp),
-                    value = "",
-                    onValueChange = { },
+                    value = nomeState.value,
+                    onValueChange = {
+                        nomeState.value = it
+                    },
                     label = {
                         Text(text = "digite seu nome")
-
-                    }
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 12.dp),
+                    leadingIcon = {
+                        Icon(
+                            ImageVector= Icons.Default.Email,
+                            contentDescription = "",
+                            tint = Color.Black
+                        )
+                    },
+                    trailingIcon = {
+                        Icon(
+                            imageVector = Icons.Default.LocationOn,
+                            contentDescription = "",
+                            tint = Color.Black
+                        )
+                    },
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Text,
+                        capitalization = KeyboardCapitalization.Words
+                    )
                 )
                 Button(
                     onClick = {},
@@ -120,6 +150,10 @@ fun HomeScreens(modifier: Modifier = Modifier) {
          }
         }
     }
+}
+
+fun Icon(ImageVector: ImageVector, contentDescription: String, tint: Color) {
+
 }
 
 @Preview(showSystemUi = true)
