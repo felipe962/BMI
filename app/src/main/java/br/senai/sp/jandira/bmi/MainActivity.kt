b8685ce7913a4b396f07196d4d7820f1dd30e985
FELIPE BAHIA CORREA <24122657@dsn127m1249386.cfp127aluno.local>
2025-03-11 17:01:01 -0300

    novas telas

--- app/src/main/java/br/senai/sp/jandira/bmi/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/bmi/MainActivity.kt
@@ -3,18 +3,20 @@ package br.senai.sp.jandira.bmi
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import br.senai.sp.jandira.bmi.screens.HomeScreens
 import br.senai.sp.jandira.bmi.ui.theme.BMITheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             BMITheme {
-                HomeScreens()
+                useDataScreen()
             }
         }
     }
+
+    private fun useDataScreen() {
+        TODO("Not yet implemented")
+    }
+
 }
